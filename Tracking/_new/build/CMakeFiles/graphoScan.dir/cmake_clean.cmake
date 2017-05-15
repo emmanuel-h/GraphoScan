d9670f3949dfb83e1d0173c37482b2b22c4594b9
ThibCharpy <thibault.charpignon@wanadoo.fr>
2017-05-15 17:42:50 +0200

    bonsoir

---
+++ Tracking/_new/build/CMakeFiles/graphoScan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphoScan.dir/main.cpp.o"
+  "graphoScan.pdb"
+  "graphoScan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphoScan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
