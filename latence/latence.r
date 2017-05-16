5d53d75b8f9bc4653331574bf6c6215dec62066b
benoitgallet <benoit.gallet@live.fr>
2017-05-16 12:58:58 +0200

    updated plot

--- latence/latence.r
+++ latence/latence.r
@@ -4,4 +4,4 @@ dataPar = data[which(data$category=="par"),]
 data3 <- cbind(dataSeq[,3], dataPar[,3])
 colnames(data3) <- c("Séquentiel", "Parallèle")
 rownames(data3) <- c(1:10)
-barplot(t(data3), beside=T, xlab="Observations", ylab="Latence (ms)", cex.names=0.8, las=2, ylim=c(0,12), col=c("darkblue", "red"))
+barplot(t(data3), main="Latence mesurée sur une capture d'image stéréo", beside=T, xlab="Observations", ylab="Latence (ms)", cex.names=0.8, las=2, ylim=c(0,12), col=c("darkblue", "red"))
