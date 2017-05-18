d9432dfef6b3a07d5c86cbceb426e17e3af54546
martin-r <martin2994@hotmail.fr>
2017-05-18 14:57:04 +0200

    Doc for Camera

--- Tracking/_new/Camera.hpp
+++ Tracking/_new/Camera.hpp
@@ -8,7 +8,6 @@
  * \brief possible movement of the Camera
  *
  */
-
 enum Camera_Mouvement {
   FORWARD,     /*!< move forward */
   BACKWARD,    /*!< move backward */
@@ -25,6 +24,9 @@ const GLfloat ZOOM = 45.0f;
 /*!
  * \class Camera
  * \brief class for Camera management
+ *
+ * Class which initialize Camera and generate PVM matrix for 3D reconstruction
+ *
  */
 class Camera {
 public:
@@ -41,9 +43,18 @@ public:
   GLfloat MovementSpeed;
   GLfloat MouseSensitivity;
   GLfloat Zoom;
-  
-  //Constructor with vectors
-  Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),GLfloat yaw = YAW, GLfloat pitch = PITCH) :mFront(glm::vec3(0.0f, 0.0f, -1.0f)),MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM){
+
+  /*!
+   * \brief Constructor
+   * 
+   * Class constructor with vector
+   *
+   * \param position new position vector
+   * \param up 
+   * \param yaw yaw angles
+   * \param pitch pitch angles
+   */
+  Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),GLfloat yaw = YAW, GLfloat pitch = PITCH) : mFront(glm::vec3(0.0f, 0.0f, -1.0f)),MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM){
      mPosition = position;
   mWorldUp = up;
   mYaw = yaw;
@@ -51,30 +62,94 @@ public:
   updateCameraVectors();
   };
   
-  //Constructor with salar values
-  Camera(GLfloat xpos, GLfloat ypos, GLfloat zpos, GLfloat xup, GLfloat yup, GLfloat zup, GLfloat yaw = YAW, GLfloat pitch = PITCH) :mFront(glm::vec3(0.0f, 0.0f, -1.0f)),MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM){
+  /*!
+   * \brief Constructor
+   * 
+   * Class constructor with scalar values
+   *
+   * \param xpos x coordinate of postion vector
+   * \param ypos y coordinate of postion vector
+   * \param zpos z coordinate of postion vector
+   * \param xup x coordinate of up vector
+   * \param yup y coordinate of up vector
+   * \param yaw yaw angles
+   * \param pitch pitch angles
+   */
+  Camera(GLfloat xpos, GLfloat ypos, GLfloat zpos, GLfloat xup, GLfloat yup, GLfloat zup, GLfloat yaw = YAW, GLfloat pitch = PITCH) : mFront(glm::vec3(0.0f, 0.0f, -1.0f)),MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM){
 mPosition = glm::vec3(xpos, ypos, zpos);
   mWorldUp = glm::vec3(xup, yup, zup);
   mYaw = yaw;
   mPitch = pitch;
   updateCameraVectors();
   };
-  
-  //Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+
+  /*!
+   * \brief Create view matrix
+   *
+   * Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+   *
+   * \return view matrix
+   */
   glm::mat4 GetViewMatrix();
-  //Returns the projection matrix calculated using Zoom and the Projective Matrix
+
+  /*!
+   * \brief Create projection matrix
+   *
+   * Returns the projection matrix calculated using Zoom and the Projective Matrix
+   *
+   * \param height window height
+   * \param width window width
+   * \return projection matrix
+   */
   glm::mat4 GetProjectionMatrix(float height = 800, float width = 600);
-  //My own lookat function
+
+  /*!
+   * \brief Calculate look at
+   *
+   * Calculate my own look at and return it
+   *
+   * \return window look at
+   */
   glm::mat4 myLookAt();
 
-  //Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+  /*!
+   * \brief Update camera on press keyboard
+   *
+   * Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+   *
+   * \param direction camera direction
+   * \param deltaTime time
+   */
   void ProcessKeyboard(Camera_Mouvement direction, GLfloat deltaTime);
 
-  //Processe input received from a mouse input system. Excepts the offset value in both the x and y direction.
+  /*!
+   * \brief Update camera on mouse movements
+   *
+   * Processe input received from a mouse input system. Excepts the offset value in both the x and y direction.
+   *
+   * \param xoffset x coordinate of mouse offset
+   * \param yoffset y coordinate of mouse offset
+   * \param constrainPitch test if pitch is out of bounds
+   */
   void ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constrainPitch = true);
-  //Precesse input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+
+  /*!
+   * \brief Update camera on mouse scroll-wheel up/down
+   *
+   * Precesse input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+   *
+   * \param yoffset y cordinate of mouse offset on scroll
+   */
   void ProcessMouseScroll(GLfloat yoffset);
+  
 private:
-  //Calculates the front vector from the Camera's (updated) Eular Angles
+  
+  /*!
+   * \brief update camera parameters
+   *
+   * Calculates the front vector from the Camera's (updated) Eular Angles
+   *
+   */
   void updateCameraVectors();
+  
 };
